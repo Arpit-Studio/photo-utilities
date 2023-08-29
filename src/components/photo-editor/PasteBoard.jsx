2c79095a96671c32e09626282d47c6134d4b3150
Sovit Tamrakar <sovit.tamrakar@gmail.com>
2023-08-29 14:31:56 +0545

    gg

--- src/components/photo-editor/PasteBoard.jsx
+++ src/components/photo-editor/PasteBoard.jsx
@@ -1,7 +1,7 @@
 export default function PasteBoard({ handlePaste }) {
   return (
     <div
-      className="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800 flex justify-center items-center h-36"
+      className="col-span-12 min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800 flex justify-center items-center h-36"
       onPaste={handlePaste}>
       <p className="text-gray-600 dark:text-gray-400 font-bold">
         Ctrl + V to paste image from clipboard
